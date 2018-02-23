77d35b2399bedc371c20fae90844de71e7d4e70a
“Mohammed <mohammedalsaihati@mail.weber.edu>
2018-02-23 11:53:06 -0700

    First commit

---
+++ LoginFunctionalityMVC/Controllers/HomeController.cs
@@ -0,0 +1,262 @@
+﻿using LoginFunctionalityMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+
+namespace LoginFunctionalityMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DBEntities db = new DBEntities();
+        private static int saltLengthLimit = 32;
+
+        #region --> Login GET Method 
+
+        [HttpGet]
+        public ActionResult Login(string returnURL)
+        {
+            var userinfo = new LoginVM();
+
+            try
+            {
+                // We do not want to use any existing identity information
+                EnsureLoggedOut();
+
+                // Store the originating URL so we can attach it to a form field
+                userinfo.ReturnURL = returnURL;
+
+                return View(userinfo);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region --> Pre Required Method For Login
+
+        //GET: EnsureLoggedOut
+        private void EnsureLoggedOut()
+        {
+            // If the request is (still) marked as authenticated we send the user to the logout action
+            if (Request.IsAuthenticated)
+                Logout();
+        }
+
+        //POST: Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            try
+            {
+                // First we clean the authentication ticket like always
+                //required NameSpace: using System.Web.Security;
+                FormsAuthentication.SignOut();
+
+                // Second we clear the principal to ensure the user does not retain any authentication
+                //required NameSpace: using System.Security.Principal;
+                HttpContext.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+
+                Session.Clear();
+                System.Web.HttpContext.Current.Session.RemoveAll();
+
+                // Last we redirect to a controller/action that requires authentication to ensure a redirect takes place
+                // this clears the Request.IsAuthenticated flag since this triggers a new request
+                return RedirectToLocal();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //GET: RedirectToLocal
+        private ActionResult RedirectToLocal(string returnURL = "")
+        {
+            try
+            {
+                // If the return url starts with a slash "/" we assume it belongs to our site
+                // so we will redirect to this "action"
+                if (!string.IsNullOrWhiteSpace(returnURL) && Url.IsLocalUrl(returnURL))
+                    return Redirect(returnURL);
+
+                // If we cannot verify if the url is local to our host we redirect to a default location
+                return RedirectToAction("Index", "Dashboard");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //GET: SignInAsync   
+        private void SignInRemember(string userName, bool isPersistent = false)
+        {
+            // Clear any lingering authencation data
+            FormsAuthentication.SignOut();
+
+            // Write the authentication cookie
+            FormsAuthentication.SetAuthCookie(userName, isPersistent);
+        }
+
+        #endregion
+
+        #region --> Login POST Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginVM entity)
+        {
+            string OldHASHValue = string.Empty;
+            byte[] SALT = new byte[saltLengthLimit];
+
+            try
+            {
+                using (db = new DBEntities())
+                {
+                    // Ensure we have a valid viewModel to work with
+                    if (!ModelState.IsValid)
+                        return View(entity);
+
+                    //Retrive Stored HASH Value From Database According To Username (one unique field)
+                    var userInfo = db.UserMasters.Where(s => s.Username == entity.Username.Trim()).FirstOrDefault();
+
+                    //Assign HASH Value
+                    if (userInfo != null)
+                    {
+                        OldHASHValue = userInfo.HASH;
+                        SALT = userInfo.SALT;
+                    }
+
+                    bool isLogin = CompareHashValue(entity.Password, entity.Username, OldHASHValue, SALT);
+
+                    if (isLogin)
+                    {
+                        //Login Success
+                        //For Set Authentication in Cookie (Remeber ME Option)
+                        SignInRemember(entity.Username, entity.isRemember);
+
+                        //Set A Unique ID in session
+                        Session["UserID"] = userInfo.UserID;
+
+                        // If we got this far, something failed, redisplay form
+                        // return RedirectToAction("Index", "Dashboard");
+                        return RedirectToLocal(entity.ReturnURL);
+                    }
+                    else
+                    {
+                        //Login Fail
+                        TempData["ErrorMSG"] = "Access Denied! Wrong Credential";
+                        return View(entity);
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        #endregion
+
+        #region --> Generate SALT Key
+
+        private static byte[] Get_SALT()
+        {
+            return Get_SALT(saltLengthLimit);
+        }
+
+        private static byte[] Get_SALT(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+
+            //Require NameSpace: using System.Security.Cryptography;
+            using (var random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+
+        #endregion
+
+        #region --> Generate HASH Using SHA512
+        public static string Get_HASH_SHA512(string password, string username, byte[] salt)
+        {
+            try
+            {
+                //required NameSpace: using System.Text;
+                //Plain Text in Byte
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(password + username);
+
+                //Plain Text + SALT Key in Byte
+                byte[] plainTextWithSaltBytes = new byte[plainTextBytes.Length + salt.Length];
+
+                for (int i = 0; i < plainTextBytes.Length; i++)
+                {
+                    plainTextWithSaltBytes[i] = plainTextBytes[i];
+                }
+
+                for (int i = 0; i < salt.Length; i++)
+                {
+                    plainTextWithSaltBytes[plainTextBytes.Length + i] = salt[i];
+                }
+
+                HashAlgorithm hash = new SHA512Managed();
+                byte[] hashBytes = hash.ComputeHash(plainTextWithSaltBytes);
+                byte[] hashWithSaltBytes = new byte[hashBytes.Length + salt.Length];
+
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    hashWithSaltBytes[i] = hashBytes[i];
+                }
+
+                for (int i = 0; i < salt.Length; i++)
+                {
+                    hashWithSaltBytes[hashBytes.Length + i] = salt[i];
+                }
+
+                return Convert.ToBase64String(hashWithSaltBytes);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+        #endregion
+
+        #region --> Comapare HASH Value
+        public static bool CompareHashValue(string password, string username, string OldHASHValue, byte[] SALT)
+        {
+            try
+            {
+                string expectedHashString = Get_HASH_SHA512(password, username, SALT);
+
+                return (OldHASHValue == expectedHashString);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        //Refer(SALT): http://codereview.stackexchange.com/questions/93614/salt-generation-in-c`  
+        //Refer(SHA512): http://zurb.com/forrst/posts/C_SHA512_Encryption_Hashing-UaL
+        //Refer 1:  https://www.mssqltips.com/sqlservertip/4037/storing-passwords-in-a-secure-way-in-a-sql-server-database/
+        //Refer 2: https://crackstation.net/hashing-security.htm
+
+    }
+}
---
+++ LoginFunctionalityMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginFunctionalityMVC.MvcApplication" Language="C#" %>
